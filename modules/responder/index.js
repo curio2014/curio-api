16184b8cadbba47645fb3c7255176337c181438a
Jesse Yang <jyyjcc@gmail.com>
2014-04-17 12:35:47 +0800

    more consise responder save

--- modules/responder/index.js
+++ modules/responder/index.js
@@ -2,18 +2,15 @@ var mesa = require_('serve/mesa')
 var Responder = require_('models/responder')
 var ERRORS = require_('models/errors')
 
-function* updateResponder() {
-  data = this.req.body
-  var responder = new Responder(data)
-  var valid = responder.validate()
-  this.assert(valid, ERRORS.BAD_REQUEST, responder.errors)
-  yield responder.save()
-  this.body = { ok: true }
-}
-
 mesa.rest('/medias/:id/responder')
   .use(mesa.auth.need('mediaAdmin'))
   .get(function* getResponder() {
       this.body = yield Responder.get(this.params.id)
   })
-  .post(updateResponder)
+  .post(function* updateResponder() {
+    var responder = new Responder(this.req.body)
+    this.assert(responder.validate(), ERRORS.BAD_REQUEST, responder.errors)
+    yield responder.save()
+    this.body = { ok: true }
+  })
+
