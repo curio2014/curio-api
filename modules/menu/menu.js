f1e27be0b9c6c00186e9832ae7edf2cdb789fa92
LiFangming <fangming.li@curiositychina.com>
2014-05-06 13:53:26 +0800

    menu module

---
+++ modules/menu/menu.js
@@ -0,0 +1,72 @@
+/**
+ * 微信自定义菜单数据处理
+ */
+var Media = require_('models/media')
+var log = require_('lib/utils/logger').log('menu')
+var store = require_('lib/store')('menu')
+
+module.exports = Menu
+
+/**
+ * 菜单
+ */
+function Menu(data) {
+  if (!isNaN(Number(data))) {
+    data = { media_id: data }
+  }
+  this._media_id = data.media_id;
+  this._menu="";
+}
+
+/**
+ * 加载菜单
+ */
+Menu.prototype.load = function* () {
+  this._menu = yield store.get(this._media_id)
+}
+
+/**
+ * 清除菜单
+ */
+Menu.prototype.destroy = function() {
+  return Responder.clear(this._media_id)
+}
+
+/**
+ * on dump customed wx menus when do toJSON
+ */
+Menu.prototype.toJSON = function() {
+  return {
+    media_id: this._media_id,
+    menu: this._menu
+  };
+}
+
+
+/**
+ * Get menu by media id
+ */
+Menu.get = function* (media_id) {
+  var menu = new Menu(media_id);
+  yield menu.load();
+  return menu;
+}
+
+/**
+ * Save customed menu to storage
+ *
+ * @param menu, an json string of webot menu
+ */
+Menu.dump = function(media_id, menu) {
+  if (!menu || menu.length <= 0) {
+    throw new Error('Menu can not empty')
+  }
+  return store.set(media_id, menu)
+}
+
+/**
+ * Clear customed menu
+ */
+Menu.destroy = function(media_id) {
+  return store.del(media_id)
+}
